99e74e26238959ccdc80fa89984525dee63b5a33
Krishna Chaitanya Muppaneni <krishna.c09@gmail.com>
2016-01-03 18:48:22 -0600

    Added files

---
+++ SplitWisely/Converter/ExpandViewerConverters/AllDebtAmountConverter.cs
@@ -0,0 +1,41 @@
+﻿using SplitWisely.Model;
+using SplitWisely.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace SplitWisely.Converter.ExpandViewerConverters
+{
+    public class AllDebtAmountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string amount = "";
+            ExpandableListModel expandableModel = value as ExpandableListModel;
+            List<Debt_Group> allDebts = expandableModel.debtList;
+            double finalBalance = Helpers.getUserGroupDebtAmount(allDebts, expandableModel.groupUser.id);
+
+            //if final balance is 0, then anyways we are not shwoing the balance.
+            if (finalBalance != 0)
+            {
+                string currency = allDebts[0].currency_code;
+                amount = currency + String.Format("{0:0.00}", Math.Abs(finalBalance));
+            }
+
+            return amount;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SplitWisely/Converter/ExpandViewerConverters/SpecificDebtAmountConverter.cs
@@ -0,0 +1,33 @@
+﻿using SplitWisely.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace SplitWisely.Converter.ExpandViewerConverters
+{
+    public class SpecificDebtAmountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string amount = "";
+            Debt_Group specificDebt = value as Debt_Group;
+            double finalBalance = System.Convert.ToDouble(specificDebt.amount, System.Globalization.CultureInfo.InvariantCulture);
+
+            string currency = specificDebt.currency_code;
+            amount = currency + String.Format("{0:0.00}", Math.Abs(finalBalance));
+
+            if (specificDebt.ownerId == specificDebt.from)
+                return amount + " to " + specificDebt.toUser.name;
+            else
+                return amount + " by " + specificDebt.fromUser.name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
