33a7176fd95446a7b8628533476ad686cfa67175
Krishna Chaitanya Muppaneni <krishna.c09@gmail.com>
2016-12-27 00:03:29 -0600

    Updated AdMediator SDK and Nuget packages

--- SplitBook/Controls/AdControl.xaml.cs
+++ SplitBook/Controls/AdControl.xaml.cs
@@ -21,43 +21,47 @@ namespace SplitBook.Controls
 {
     public sealed partial class AdControl : UserControl
     {
+        private Microsoft.Advertising.WinRT.UI.AdControl AdMediator;
         public AdControl()
         {
+            AdMediator = new Microsoft.Advertising.WinRT.UI.AdControl();
+            AdMediator.ErrorOccurred += AdMediator_ErrorOccurred;
             this.InitializeComponent();
-        }
-
-        private void AdMediator_AdMediatorError(object sender, Microsoft.AdMediator.Core.Events.AdMediatorFailedEventArgs e)
-        {
-            AdMediator.Visibility = Visibility.Collapsed;
-        }
-
-        private void AdMediator_AdSdkError(object sender, Microsoft.AdMediator.Core.Events.AdFailedEventArgs e)
-        {
-            AdMediator.Visibility = Visibility.Collapsed;
         }
 
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
             if (Advertisement.ShowAds)
             {
-                AdMediator.Visibility = Visibility.Visible;                
-                if(AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop")
+                AdMediator.Visibility = Visibility.Visible;
+                if (AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
                 {
+                    AdMediator.ApplicationId = "39ee0609-be6d-4158-b211-5b83d6ec32c3";
+                    AdMediator.AdUnitId = "11664988";
+                    AdMediator.Width = 480;
+                    AdMediator.Height = 80;
+                }
+                else
+                {
+
+                    AdMediator.ApplicationId = "504c2e83-08d6-405f-a2a5-da731188fd85";
+                    AdMediator.AdUnitId = "11664987";
                     AdMediator.Width = 728;
-                    AdMediator.Height = 90;                   
+                    AdMediator.Height = 90;
                 }
-                else if(AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Mobile")
-                {
-                    AdMediator.Width = 320;
-                    AdMediator.Height = 50;
-                }
-                AdMediator.Disable();
-                AdMediator.Resume();
+                AdMediator.IsAutoRefreshEnabled = true;
+                adGrid.Children.Add(AdMediator);
             }
             else
             {
-                Visibility = Visibility.Collapsed;                
-            }            
+                Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void AdMediator_ErrorOccurred(object sender, Microsoft.Advertising.WinRT.UI.AdErrorEventArgs e)
+        {
+            AdMediator.Visibility = Visibility.Collapsed;
+            AdMediator.IsAutoRefreshEnabled = false;
         }
     }
 }
