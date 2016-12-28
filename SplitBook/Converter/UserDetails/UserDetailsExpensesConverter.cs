560aee1e7c0670c1a1c08281a4c7963f447ae951
Krishna Chaitanya Muppaneni <krishna.c09@gmail.com>
2016-12-27 22:33:24 -0600

    Currency code in UserDetails is replaced by currency symbol

--- SplitBook/Converter/UserDetails/UserDetailsExpensesConverter.cs
+++ SplitBook/Converter/UserDetails/UserDetailsExpensesConverter.cs
@@ -1,6 +1,8 @@
-﻿using SplitBook.Model;
+﻿using SplitBook.Controller;
+using SplitBook.Model;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +16,25 @@ namespace SplitBook.Converter.UserDetails
         {
             string description = "settled up";
             double finalBalance = 0;
+            string currencyCode = String.Empty;
+            string amount = String.Empty;
+            QueryDatabase query = new QueryDatabase();
             Balance_User balance = value as Balance_User;
             finalBalance = System.Convert.ToDouble(balance.amount, System.Globalization.CultureInfo.InvariantCulture);
-            string amount = balance.currency_code + String.Format("{0:0.00}", Math.Abs(finalBalance));
+            if (App.currentUser != null)
+                currencyCode = query.getUnitForCurrency(App.currentUser.default_currency.ToUpper());
+            if (currencyCode != String.Empty)
+            {
+                var format = (NumberFormatInfo)NumberFormatInfo.CurrentInfo.Clone();
+                format.CurrencySymbol = currencyCode;
+                format.CurrencyNegativePattern = 1;
+                amount = String.Format(format, "{0:C}", Math.Abs(finalBalance));
+
+            }
+            else
+            {
+                amount = balance.currency_code + String.Format("{0:0.00}", Math.Abs(finalBalance));
+            }
             
             if (finalBalance > 0)
                 description = "owes you " + amount;
